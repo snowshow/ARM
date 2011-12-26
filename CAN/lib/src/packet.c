ea3f256a7ebd35c12f84411994a7f66f2365f4dc
Élie Bouttier <cube.elie@gmail.com>
2011-12-27 00:38:37 +0100

    CAN_packet function

--- CAN/lib/src/packet.c
+++ CAN/lib/src/packet.c
@@ -1,25 +1,71 @@
 #include <stdarg.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
 
-int CAN_packet(uint8_t *packet, int id, int length, ...)
+#include "can.h"
+
+can_t* CAN_packet(int id, int length, ...)
 {
     va_list va;
     int i;
+	int b;
 
     if (length > 8)
-        return -1;
+        return NULL;
 
-    va_start(va, length);
-    packet[0] = 0xFD;
-    packet[1] = id >> 8;
-    id %= 256;
-    packet[2] = id;
-    packet[1] |= length << 4;
-    for (i = 0 ; i < length ; i++) {
-        packet[i+3] = va_arg(va, int);
-    }
-    packet[length+3] = 0xBF;
-    va_end(va);
+	can_t* packet = NULL;
+	if ((packet = malloc(sizeof(can_t))) == NULL) {
+		errno = ENOMEM;
+		return NULL;
+	}
+	
+	if (id > 2048 || id < 0) {
+		errno = EINVAL;
+		return NULL;
+	}
+	packet->id = id;
+	if (length > 8 || length < 0) {
+		errno = EINVAL;
+		return NULL;
+	}
+	packet->length = length;
+	packet->data = NULL;
 
-    return 0;
+	if((packet->data = malloc(length * sizeof(uint8_t))) == NULL) {
+		errno = ENOMEM;
+		return NULL;
+	}
+
+	va_start(va, length);
+	for (i = 0 ; i < length ; i++) {
+		b = (uint8_t) va_arg(va, int);
+		if (b > 255 || b < 0) {
+			errno = EINVAL;
+			return NULL;
+		}
+		packet->data[i] = b;
+	}
+	va_end(va);
+
+	return packet;
 }
+
+//can_t* truc(void) {
+//
+//    va_start(va, length);
+//    packet->data[0] = 0xFD;
+//    packet->data[1] = id >> 8;
+//    id %= 256;
+//    packet->data[2] = id;
+//    packet->data[1] |= length << 4;
+//    for (i = 0 ; i < length ; i++) {
+//        packet->data[i+3] = va_arg(va, int);
+//    }
+//    packet->data[length+3] = 0xBF;
+//    va_end(va);
+//
+//    return packet;
+//}
+
