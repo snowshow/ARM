acf33c7a5511e449c61d1da3dbdb14d66acf52a8
Elie Bouttier <cube.elie@gmail.com>
2012-01-03 21:46:51 +0100

    Doc du include du can.h (début)

--- CAN/lib/include/can.h
+++ CAN/lib/include/can.h
@@ -1,8 +1,9 @@
 #ifndef _CAN_H
 #define _CAN_H
 
-#include <stdint.h>
+#include <stdint.h> /* Pour uint8_t */
 
+/** Définition de la structure can_t représentant les packets CAN */
 typedef struct {
 	int id;
 	int length;
@@ -16,10 +17,20 @@ typedef struct {
 	uint8_t b8;
 } can_t;
 
+/** Initialize la structure can_t à partir des informations passé en argument.
+ * @param packet Pointeur sur le packet à initialiser.
+ * @param id Identifiant du packet (de 0 à 2047 selon la norme CAN).
+ * @param length Nombre d'octet de donnée (de 0 à 8 selon la norme CAN).
+ * Paramètre supplémentaire : de 0 à 8 uint8_t, 1 par octet de donnée. */
+int CAN_packet(can_t * packet, int id, int length, ...);
+
+/** Définition de l'enum can_f (can format) représentant les différents format
+ * de données représentant les informations circulant sur le bus CAN */
 typedef enum {
 	bin,
 	dec,
-	hex
+	hex,
+	txt
 } can_f;
 
 void CAN_set(can_t * packet, int b, uint8_t c);
@@ -27,27 +38,7 @@ int CAN_on_event(int mask, int filter, void (*event)(can_t));
 int CAN_listen_on(int fd, can_f);
 uint8_t CAN_get(can_t const * packet, int b);
 
-/** Renvoit une structure can_t initialisé à partir des données passées en
- * arguments. Échoue si l'espace mémoire disponible est insuffisant. Renvoit
- * alors NULL et errno contient ENOMEM.
- * @param id Id du message CAN.
- * @param length Nombre d'octets de donnée du packet.
- */
-int CAN_packet(can_t * packet, int id, int length, ...);
 
-/** Écrit le message can packet sur le file descriptor fd
- * @param fd File descriptor sur lequel sera écrit le packet
- * @param packet Structure contenant les données à transmettre.
- * @return 0 en cas de succès, -1 en cas d'échec (consulter errno)
- */
 int CAN_write(int fd, can_t const * packet, int format);
-
-/** Enregistre une fonction qui sera appelé en cas de données can arrivant sur
- * le file descriptor passé en argument.
- * @param fd File descriptor en écoute
- * @param event Pointeur sur une fonction du type void (*event)(can_t*) qui sera
- * appelé en cas de réception de données CAN.
- */
-//int CAN_recv(int fd, int mask, void (*event)(can_t));
 
 #endif
